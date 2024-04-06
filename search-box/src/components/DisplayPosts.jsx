3c076e8db74f65fa64ef921c933e9cb88b78cd04
chehab <user.chehabb2003@gmail.com>
2024-04-06 18:07:48 +0300

    working project without styling

--- search-box/src/components/DisplayPosts.jsx
+++ search-box/src/components/DisplayPosts.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+
 const DisplayPosts = ({searchValue}) => {
-    const articles = [{title: 'Css Grid', date: 'Oct 09, 2018', content:'Just Testing the Css Grid'}]
-    const filteredArticles = articles.map((article) => {
+    const articles = [{ title: 'Css Grid', date: 'Oct 09, 2018', content: 'Just Testing the Css Grid' },
+    { title: 'Css Grid2', date: 'Oct 09, 2018', content: 'Just Testing the Css Grid2' },
+    ]
+
+    const [matchedCount, setMatchedCount] = useState(0);
+
+    const filteredArticles = articles.filter(article => 
         article.title.toLowerCase().includes(searchValue.toLowerCase()) ||
         article.content.toLowerCase().includes(searchValue.toLowerCase())
-    })
+    )
+    console.log(filteredArticles);
+
+     useEffect(() => {
+        setMatchedCount(filteredArticles.length);
+    },[searchValue])
+
+
 
     const highightText = (text, hightlightWord) => {
         const partsOfText = text.split(new RegExp(`(${hightlightWord})`, 'gi'))
@@ -19,13 +33,23 @@ const DisplayPosts = ({searchValue}) => {
 
     return (
         <div className="articles">
-            {filteredArticles.map((article) => {
-                <div className="article">
-                    <h1 className="title">{highightText(article.title, searchValue)}</h1>
-                    <p className="Date">{article.date}</p>
-                    <div className="article-content">{highightText(article.content, searchValue)}</div>
-                </div>
-            })}
+            {filteredArticles.length > 0 && searchValue !== '' & searchValue!== ' ' ? (
+                <div>
+                    {matchedCount > 1 ? <div>{`${matchedCount} posts were found`}</div> : (
+                        <div>{`${matchedCount} post was found`}</div>
+                    )}
+                    {filteredArticles.map(article => 
+                        <div className="article">
+                            <h1 className="title">{highightText(article.title, searchValue)}</h1>
+                            <p className="Date">{article.date}</p>
+                            <div className="article-content">{highightText(article.content, searchValue)}</div>
+                        </div>
+                    )}
+                    </div>    
+            ) : (
+                    <div></div>
+            )}
+
         </div>
     )
 }
